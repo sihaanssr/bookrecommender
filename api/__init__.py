060c120a57d4231228306ed507cc4c2a06af87b0
Anup Joseph <anup20joseph@gmail.com>
2020-02-02 18:58:57 +0530

    Backend Completed

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+from logging import logger
+from flask import Flask
+def create_app():
+    app = Flask(__name__)
+    from .app import main
+    app.register_blueprint(main)
+    logger.debug("App registered")
+    return app
